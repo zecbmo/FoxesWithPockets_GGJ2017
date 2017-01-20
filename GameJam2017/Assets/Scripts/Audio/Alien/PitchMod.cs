daaefc9ebd1bf4ef3d5e1d20d411fbc61190d544
DanLaptop <daniel.m.allan@googlemail.com>
2017-01-20 23:17:50 +0000

    playing with audio mixers: set up base mixer, played with exposing effect variables, tested changing audio mixer groups

---
+++ GameJam2017/Assets/Scripts/Audio/Alien/PitchMod.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PitchMod : MonoBehaviour {
+
+    [SerializeField]
+    AudioMixerGroup selectedGroup;
+
+    [SerializeField]
+    AudioMixer baseMixer;
+    AudioSource mySource;
+
+    float pitchMod;
+	// Use this for initialization
+	void Start () {
+        mySource = GetComponent<AudioSource>();
+        mySource.outputAudioMixerGroup = selectedGroup;
+    }
+	
+	// Update is called once per frame
+	void Update () {   
+	}
+
+    private void PitchModFunction()
+    {
+        pitchMod += Random.Range(0.04f, 0.1f);
+        if (pitchMod > 2.0) pitchMod = 0.3f;
+
+           baseMixer.SetFloat("alienPitch",pitchMod);
+    }
+
+    private bool randomBool() { return (Random.value > 0.5f); }
+}
