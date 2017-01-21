0de66d2fb0ed94a3e7c58aa1ae4b5e029d4a9622
zecbmo <gmccartan05@gmail.com>
2017-01-21 14:13:59 +0000

    Feat_AnswerBin And Answers Added

---
+++ GameJam2017/Assets/Scripts/AnswerBin.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnswerBin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public delegate void GameWin();
+    public static event GameWin OnGameWin;
+
+    public delegate void GameLose();
+    public static event GameLose OnGameLose;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Answer"))
+        {
+            AnswerBlock Answer = other.gameObject.GetComponent<AnswerBlock>();
+
+            if (Answer.CorrectAnswer)
+            {
+                OnGameWin();
+            }
+            else
+            {
+                OnGameLose();
+            }
+        }
+    }
+
+}
---
+++ GameJam2017/Assets/Scripts/AnswerBlock.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnswerBlock : MonoBehaviour {
+
+    // Use this for initialization
+    public bool CorrectAnswer = false;
+}
