3fa779cc7445b96ec5b6621ce48c732829661be5
zecbmo <gmccartan05@gmail.com>
2017-02-09 16:06:49 +0000

    Fixed for VR

---
+++ GameJam2017/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadSceneAsync(1);
+                
+        }
+
+		
+	}
+}
